e897e06ffea2439c09850abe91c85ef2b526e0ab
Giang Nguyen <hust2412@gmail.com>
2019-08-07 22:09:06 -0400

    Scale to validation dataset

--- tutorials/03-advanced/image_captioning/split_mscoco.py
+++ tutorials/03-advanced/image_captioning/split_mscoco.py
@@ -6,54 +6,59 @@ from shutil import copyfile
 def split_data(json_path, train=True):
 
     if train:
-        with open(json_path) as json_file:
-            data = json.load(json_file)
-            categories = data['categories']
+        dataset = 'train'
+    else:
+        dataset = 'val'
 
-            image_num = len(data['images'])
+    with open(json_path) as json_file:
+        data = json.load(json_file)
+        categories = data['categories']
 
-            classes = dict()  # Contains 80 classes of MS-COCO, key is class id
-            for category in categories:
-                classes[category['id']] = category['name']
+        image_num = len(data['images'])
 
-            for key, value in classes.items():
-                if not os.path.exists('train2014/{}'.format(value)):
-                    os.mkdir('train2014/{}'.format(value))
+        classes = dict()  # Contains 80 classes of MS-COCO, key is class id
+        for category in categories:
+            classes[category['id']] = category['name']
 
-            annotations = data['annotations']
-            print(image_num, len(annotations))
+        for key, value in classes.items():
+            if not os.path.exists('{}2014/{}'.format(dataset, value)):
+                os.mkdir('{}2014/{}'.format(dataset, value))
 
-            annotation_dict = {}
-            image_count = 0
+        annotations = data['annotations']
+        print(image_num, len(annotations))
 
-            for i, annotation in enumerate(annotations, 1):
-                image_id = annotation['image_id']
-                file_name = ('COCO_train2014_%012d.jpg' % image_id)
-                category_id = annotation['category_id']
+        annotation_dict = {}
+        image_count = 0
 
-                dst_file = ('train2014/{}/{}'.format(classes[category_id], file_name))
-                src_file = '/home/dexter/Downloads/train2014/{}'.format(file_name)
+        for i, annotation in enumerate(annotations, 1):
+            image_id = annotation['image_id']
+            file_name = ('COCO_%s2014_%012d.jpg' % (dataset,  image_id))
+            category_id = annotation['category_id']
 
-                if image_id in annotation_dict:  # At least two annotations in an image, should delete this image
-                    if os.path.exists(dst_file):
-                        image_count -= 1
-                        os.remove(dst_file)
-                    continue  # Here to only pick images having just one class / or one annotation
-                else:
-                    annotation_dict[image_id] = 1
+            dst_file = ('{}2014/{}/{}'.format(dataset, classes[category_id], file_name))
+            src_file = '/home/dexter/Downloads/{}2014/{}'.format(dataset, file_name)
 
-                copyfile(src_file, dst_file)
+            if image_id in annotation_dict:  # At least two annotations in an image, should delete this image
+                if os.path.exists(dst_file):
+                    image_count -= 1
+                    os.remove(dst_file)
+                continue  # Here to only pick images having just one class / or one annotation
+            else:
+                annotation_dict[image_id] = 1
 
-                if os.path.exists('train2014/{}/{}'.format(classes[category_id], file_name)):  # If copy successfully
-                    #  print("File " + 'train2014/{}/{}'.format(classes[category_id], file_name) + " is copied")
-                    image_count += 1
-                else:
-                    print("Copy failed!")
+            copyfile(src_file, dst_file)
 
-                if i%1000 == 0:  # With 1k annotations parsed, we print the number of images having a single class
-                    print("{}/{} images have been copied!".format(image_count, image_num))
+            if os.path.exists('{}2014/{}/{}'.format(dataset, classes[category_id], file_name)):  # If copy successfully
+                #  print("File " + '{}2014/{}/{}'.format(dataset, classes[category_id], file_name) + " is copied")
+                image_count += 1
+            else:
+                print("Copy failed!")
 
-            print("Finish splitting dataset from {}".format(json_path))
+            if i%1000 == 0:  # With 1k annotations parsed, we print the number of images having a single class
+                print("{}/{} images have been copied!".format(image_count, image_num))
+
+        print("Finish {} splitting dataset from {}".format(dataset, json_path))
 
 
-split_data('instances_train2014.json', train=True)
+#split_data('instances_train2014.json')
+split_data('instances_val2014.json', train=False)
